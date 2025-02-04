80df7df5c59a2825fcbd27b4e31c19cfa6a3b787
thaoto22 <thaoto@oregonstate.edu>
2025-02-04 13:45:30 -0800

    2nd update

---
+++ gui.py
@@ -0,0 +1,106 @@
+import tkinter
+import os
+from tkinter import filedialog
+from pathlib import Path
+import pandas as pd
+
+
+class Tk:
+
+    def __init__(self):
+        # set up GUI window
+        self.window = tkinter.Tk()
+        self.window.title('Column Formatter')
+        self.window.geometry('750x500')
+
+        self.frame = tkinter.Frame(self.window)
+        self.frame.pack()
+
+        # Main frame to store all labels so that that app will be responsive
+        self.input_frame = tkinter.LabelFrame(self.frame, text='Enter info below')
+        self.input_frame.grid(row=0, column=0)
+
+        # Create entry
+        file_name_text = tkinter.Label(self.input_frame, text='Enter file name below:', pady=5)
+        file_name_text.grid(row=0, column=0)
+        self.file_name = tkinter.Entry(self.input_frame, width=30)
+        self.file_name.grid(row=1, column=0)
+        self.file_name.config(justify='center')
+
+        # ---------------------List box------------------------------------ #
+        self.listbox = tkinter.Listbox(self.input_frame, selectmode=tkinter.MULTIPLE)
+        self.listbox.grid(row=2, column=0, pady=5)
+
+        self.input_area = tkinter.Label(self.input_frame, text='Press "Upload" button below to load headers from CSV file', pady=10)
+        self.input_area.grid(row=3, column=0)
+
+        # ---------------------Button------------------------------------ #
+        self.button = tkinter.Button(self.input_frame, text="Upload", width=15, command=lambda: self.process_headers())
+        self.button.grid(row=4, column=0)
+
+        # ---------------------Status Message------------------------------------ #
+        self.message = tkinter.Label(self.input_frame, text="", pady=10)
+        self.message.grid(row=5, column=0)
+
+    def process_headers(self):
+        """
+        method to allow users to select path where csv file is located
+        """
+        path = filedialog.askopenfilename()
+
+        if not path:
+            pass
+
+        # Check if name entry field is empty
+        elif len(self.file_name.get()) == 0:
+            message = ('Missing: file name')
+            self.get_status(message, 'red')
+
+        elif path and self.check_csv_extension(path):
+            self.path = path
+            self.get_status('Please wait while your data is being processed! \n'
+                            'You will see a list of column header names above once completed', 'brown')
+
+            # Disable entry field and upload button once we start processing data
+            self.file_name.config(state='disabled')
+            self.button.config(state='disabled')
+
+            print(self.get_header_list(self.path))
+
+        else:
+            message = ('The file you have selected is not the correct file! \n '
+                       'Make sure you are using a .csv file.\n'
+                       'Please try again!')
+            self.get_status(message, 'red')
+
+    def get_header_list(self, path):
+        """
+        returns list of headers
+        """
+        df = pd.Dataframe(path)
+        df_headers = df.columns.tolist()
+        return df_headers
+
+    def get_status(self, message, color='black'):
+        """
+        Returns status message on window
+        """
+
+        self.message.config(text=message, fg=color, font='bold')
+
+    def check_csv_extension(self, file_path):
+        """
+        Check file path if it is a csv file
+        """
+        return Path(file_path).suffix.lower() == '.csv'
+
+    def start(self):
+        """
+        Start application
+        """
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    app = Tk()
+    app.start()
--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-# initial set up
-# test commit
+
+
